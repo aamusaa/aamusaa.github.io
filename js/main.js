95d513a1612ade8eaef26c4a6ede8f0baa6696f9
Amina Musa <aminamusaa@Aminas-MacBook-Pro.local>
2017-08-03 14:16:09 -0700

    first website load

---
+++ js/main.js
@@ -0,0 +1,26 @@
+// nav dropshadow 
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+
+// resize nav on scroll
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
+
+// nav collapse in after selection
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
